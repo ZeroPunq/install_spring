4b5ce0e4222c95bef9017f0f9f117110b963dd45
ZeroPunq <davidironhell@gmail.com>
2025-01-17 10:54:31 +0100

    Comiteo final

--- Instalacion/src/main/java/org/example/instalacion/InstalacionApplication.java
+++ Instalacion/src/main/java/org/example/instalacion/InstalacionApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InstalacionApplication {
+public class  InstalacionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InstalacionApplication.class, args);
